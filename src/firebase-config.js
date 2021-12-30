b1aaf83407704e9253ee9ef1f168ab20979571b7
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2021-12-30 22:36:33 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,14 +5,7 @@ import {getFirestore}from "@firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYWclviCodyXMuUfVs9Ah5XCFTKjZ41iE",
-    authDomain: "react-curd-84b07.firebaseapp.com",
-    databaseURL: "https://react-curd-84b07-default-rtdb.firebaseio.com",
-    projectId: "react-curd-84b07",
-    storageBucket: "react-curd-84b07.appspot.com",
-    messagingSenderId: "448810911586",
-    appId: "1:448810911586:web:75f097fd3dfbd7e17a0e3b",
-    measurementId: "G-6W0039TZ2V"
+  
   };
 
 
